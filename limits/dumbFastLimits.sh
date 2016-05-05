baa8f2df108c4a6fd862bb82e8483dafd6d04ba2
Nhan Tran <ntran@mac-127796.dhcp.fnal.gov>
2016-05-05 14:14:22 -0500

    add

---
+++ limits/dumbFastLimits.sh
@@ -0,0 +1,8 @@
+!/bin/bash
+
+combine -M Asymptotic combine_Zprime50.dat -n Zprime50
+combine -M Asymptotic combine_Zprime100.dat -n Zprime100
+combine -M Asymptotic combine_Zprime150.dat -n Zprime150
+combine -M Asymptotic combine_Zprime200.dat -n Zprime200
+combine -M Asymptotic combine_Zprime250.dat -n Zprime250
+combine -M Asymptotic combine_Zprime300.dat -n Zprime300
