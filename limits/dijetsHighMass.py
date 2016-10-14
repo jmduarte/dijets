5721a7106495bf4a7430142a2ba91eaefce04c7b
Nhan Tran <ntran@mac-127796.dhcp.fnal.gov>
2016-10-14 12:28:37 -0500

    bug fix

--- limits/dijetsHighMass.py
+++ limits/dijetsHighMass.py
@@ -10,6 +10,25 @@ ROOT.gStyle.SetPadTopMargin(0.10);
 ROOT.gStyle.SetPalette(109);
 
 
+def DMConstraintsInfty(x):
+
+	mM = x[0];
+	return 3/2./6.;
+
+def DMConstraintsM0(x):
+
+	mt = 173.;
+	mM = x[0];
+	term2 = 0.;
+	if mM > 2*mt: term2 = math.sqrt( 1 - 4.*mt*mt/mM/mM );
+
+	nf = 5 + term2;
+
+	num = (9./4.);
+	den = 1.+(16./3./nf);
+
+	return math.sqrt(num/den)/6.;
+
 def main():
 
 	# read in the data: 
@@ -35,6 +54,15 @@ def main():
 	exp2s_gq.SetFillColor(5);
 
 	#--------------------------------
+	DMM0func = ROOT.TF1("DMM0func",DMConstraintsM0,0,5000,0);
+	DMMInffunc = ROOT.TF1("DMMInffunc",DMConstraintsInfty,0,5000,0);
+	DMM0func.SetLineColor(ROOT.kGray+2);
+	DMMInffunc.SetLineColor(ROOT.kGray+2);
+	DMM0func.SetLineStyle(2);
+	DMMInffunc.SetLineStyle(3);
+	DMM0func.SetLineWidth(2);
+	DMMInffunc.SetLineWidth(2);
+
 	#--------------------------------
 	# PLOTTING
 	lowlim = 350;
@@ -59,12 +87,21 @@ def main():
 	hrl.GetYaxis().SetTitleOffset(1.0);
 	hrl.GetXaxis().SetTitle("Z\' mass (GeV)");
 
+	txt1 = ROOT.TLatex(0.20,0.58,"g_{q} = 0.25, g_{#chi} = 1, m_{#chi} = 0");
+	txt1.SetNDC(); txt1.SetTextFont(42); txt1.SetTextSize(0.03);
+	txt2 = ROOT.TLatex(0.20,0.46,"g_{q} = 0.25, g_{#chi} = 1, m_{#chi} = #infty");
+	txt2.SetNDC(); txt2.SetTextFont(42); txt2.SetTextSize(0.03);
+
 	exp2s_gq.Draw("f");
 	exp1s_gq.Draw("fsames");
 	obs_gq.Draw("l");
 	exp_gq.Draw("l");
+	DMM0func.Draw("SAMES")
+	DMMInffunc.Draw("SAMES")
 	txta.Draw();
 	txtc.Draw();
+	txt1.Draw();
+	txt2.Draw();
 	leg.Draw();
 	can_gB.SaveAs('limplotsHighMass/gB.pdf');
 
@@ -74,26 +111,42 @@ def main():
 	ROOT.gPad.SetLogx();
 	can_gB.SaveAs('limplotsHighMass/gB_logx.pdf');	
 
+def divide(iG,iXS,iGB=False,iGDM=1,iGQ=0.25,iMDM=1.):
+	for i0 in range(0,iG.GetN()):
+		iG.GetY()[i0] = iG.GetY()[i0]/iXS.Eval(iG.GetX()[i0])/(5./6.)
+		lDMWidth = avtotwidth(2,iGDM,iGQ,iG.GetX()[i0],iMDM)
+		lWidth   = avtotwidth(2,0.  ,iGQ,iG.GetX()[i0],iMDM)
+		iG.GetY()[i0] = (lWidth/lDMWidth)*iG.GetY()[i0]
+		if iGB:
+			iG.GetY()[i0]=(math.sqrt(iG.GetY()[i0]))*0.25*6
+
 def getobs(obslo,obshi,xs):
 
 	x = [];
 	y = [];
+	iGDM=1;iGQ=0.25;iMDM=1.;
 
 	for i in range( obslo.GetN() ):
 
-		factor = xs.Eval( obslo.GetX()[i] ) / (5./6.); 
+		lDMWidth = avtotwidth(2,iGDM,iGQ,obslo.GetX()[i],iMDM)
+		lWidth   = avtotwidth(2,0.  ,iGQ,obslo.GetX()[i],iMDM)
+
+		factor = xs.Eval( obslo.GetX()[i] ) / (6./5.); 
 
 		x.append( obslo.GetX()[i] );
-		y.append( math.sqrt( obslo.GetY()[i] / factor ) / 4. );
+		y.append( math.sqrt( (lWidth/lDMWidth)*obslo.GetY()[i] / factor ) / 4. );
 
 	for i in range( obshi.GetN() ):
 
+		lDMWidth = avtotwidth(2,iGDM,iGQ,obshi.GetX()[i],iMDM)
+		lWidth   = avtotwidth(2,0.  ,iGQ,obshi.GetX()[i],iMDM)		
+
 		if obshi.GetX()[i] > 3700.: break;
 
-		factor = xs.Eval( obshi.GetX()[i] ) / (5./6.);
+		factor = xs.Eval( obshi.GetX()[i] ) / (6./5.);
 
 		x.append( obshi.GetX()[i] );
-		y.append( math.sqrt( obshi.GetY()[i] / factor ) / 4. );
+		y.append( math.sqrt( (lWidth/lDMWidth)*obshi.GetY()[i] / factor ) / 4. );
 
 	obs_gr = makeAGraph( x, y );
 	
@@ -105,10 +158,14 @@ def getexp(explo,exphi,xs):
 	y = [];
 	yup = []; 
 	ydn = []; 
+	iGDM=1;iGQ=0.25;iMDM=1.;
 
 	for i in range( explo.GetN() ):
 
-		factor = xs.Eval( explo.GetX()[i] ) / (5./6.);
+		lDMWidth = avtotwidth(2,iGDM,iGQ,explo.GetX()[i],iMDM)
+		lWidth   = avtotwidth(2,0.  ,iGQ,explo.GetX()[i],iMDM)
+
+		factor = xs.Eval( explo.GetX()[i] ) / (6./5.);
 		cury   = explo.GetY()[i];
 		curyup = explo.GetY()[i] + explo.GetEYhigh()[i];
 		curydn = explo.GetY()[i] - explo.GetEYlow()[i];
@@ -117,15 +174,18 @@ def getexp(explo,exphi,xs):
 		curydn /= factor;
 
 		x.append( explo.GetX()[i] );
-		y.append( math.sqrt(cury) / 4. );
-		yup.append( math.sqrt(curyup) / 4. );
-		ydn.append( math.sqrt(curydn) / 4. );
+		y.append( math.sqrt((lWidth/lDMWidth)*cury) / 4. );
+		yup.append( math.sqrt((lWidth/lDMWidth)*curyup) / 4. );
+		ydn.append( math.sqrt((lWidth/lDMWidth)*curydn) / 4. );
 
 	for i in range( exphi.GetN() ):
 
+		lDMWidth = avtotwidth(2,iGDM,iGQ,exphi.GetX()[i],iMDM)
+		lWidth   = avtotwidth(2,0.  ,iGQ,exphi.GetX()[i],iMDM)
+
 		if exphi.GetX()[i] > 3700.: break;
 
-		factor = xs.Eval( exphi.GetX()[i] ) / (5./6.);
+		factor = xs.Eval( exphi.GetX()[i] ) / (6./5.);
 		cury   = exphi.GetY()[i];
 		curyup = exphi.GetY()[i] + exphi.GetEYhigh()[i];
 		curydn = exphi.GetY()[i] - exphi.GetEYlow()[i];
@@ -135,9 +195,9 @@ def getexp(explo,exphi,xs):
 		curydn /= factor;
 
 		x.append( exphi.GetX()[i] );
-		y.append( math.sqrt(cury) / 4. );
-		yup.append( math.sqrt(curyup) / 4. );
-		ydn.append( math.sqrt(curydn) / 4. );
+		y.append( math.sqrt((lWidth/lDMWidth)*cury) / 4. );
+		yup.append( math.sqrt((lWidth/lDMWidth)*curyup) / 4. );
+		ydn.append( math.sqrt((lWidth/lDMWidth)*curydn) / 4. );
 
 	exp_gr = makeAGraph( x, y );
 	exp_gr_band = makeAFillGraph( x, ydn, yup );
@@ -148,45 +208,45 @@ def getexp(explo,exphi,xs):
 def dijetxs():
 
 	## these cross-sections are for gq = 0.25
-    x = array('d', [])
-    y = array('d', [])
-    x.append(528.7733914858716)
-    y.append(57.269239965262344)
-    x.append(631.2905390292997)
-    y.append(26.146418729332762)
-    x.append(795.7485275479014)
-    y.append(10.943588903119819)
-    x.append(980.9839334973528)
-    y.append(4.998039360562294)
-    x.append(1166.2193394468052)
-    y.append(2.2826513012206147)
-    x.append(1331.0361216730032)
-    y.append(1.3151916426227461)
-    x.append(1578.3102214269736)
-    y.append(0.6008150098368491)
-    x.append(1825.7147916200695)
-    y.append(0.30829460763931776)
-    x.append(2073.0541265936026)
-    y.append(0.1492637583468037)
-    x.append(2217.3544322672033)
-    y.append(0.09943936422165509)
-    x.append(2454.5170729888905)
-    y.append(0.0556698333169542)
-    x.append(2701.98687840155)
-    y.append(0.03027483510044679)
-    x.append(2928.9075896518298)
-    y.append(0.018491825891702274)
-    x.append(3104.1946246179077)
-    y.append(0.011967992892333293)
-    x.append(3310.4357712666806)
-    y.append(0.007309405483957529)
-    x.append(3475.513494371131)
-    y.append(0.005313471607526083)
-    x.append(3568.343211809438)
-    y.append(0.004337281648532702)
-    lGraph    = makeAGraph( x, y, 1, 3 )
-    lGraph.SetMarkerColor(1)
-    return lGraph
+	x = array('d', [])
+	y = array('d', [])
+	x.append(528.7733914858716)
+	y.append(57.269239965262344)
+	x.append(631.2905390292997)
+	y.append(26.146418729332762)
+	x.append(795.7485275479014)
+	y.append(10.943588903119819)
+	x.append(980.9839334973528)
+	y.append(4.998039360562294)
+	x.append(1166.2193394468052)
+	y.append(2.2826513012206147)
+	x.append(1331.0361216730032)
+	y.append(1.3151916426227461)
+	x.append(1578.3102214269736)
+	y.append(0.6008150098368491)
+	x.append(1825.7147916200695)
+	y.append(0.30829460763931776)
+	x.append(2073.0541265936026)
+	y.append(0.1492637583468037)
+	x.append(2217.3544322672033)
+	y.append(0.09943936422165509)
+	x.append(2454.5170729888905)
+	y.append(0.0556698333169542)
+	x.append(2701.98687840155)
+	y.append(0.03027483510044679)
+	x.append(2928.9075896518298)
+	y.append(0.018491825891702274)
+	x.append(3104.1946246179077)
+	y.append(0.011967992892333293)
+	x.append(3310.4357712666806)
+	y.append(0.007309405483957529)
+	x.append(3475.513494371131)
+	y.append(0.005313471607526083)
+	x.append(3568.343211809438)
+	y.append(0.004337281648532702)
+	lGraph    = makeAGraph( x, y, 1, 3 )
+	lGraph.SetMarkerColor(1)
+	return lGraph
 
 ########################################################################################
 
